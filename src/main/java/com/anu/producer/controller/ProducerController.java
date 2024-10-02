12106e04a83005f61eaa07026c0aa89119f49370
byatham <balaiahy23@gmail.com>
2024-10-02 19:35:28 +0530

    first commit

---
+++ src/main/java/com/anu/producer/controller/ProducerController.java
@@ -0,0 +1,24 @@
+package com.anu.producer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("producer")
+@Slf4j
+public class ProducerController {
+	
+	@GetMapping("/getMessage/{name}")
+	public String getProducerMessage(@PathVariable("name") String name)
+	{
+		log.info("inside ProducerController:getProducerMessage");
+		log.debug("inside ProducerController:getProducerMessage");
+
+		return "Hello "+name+" Welcome To Springboot Tutorial";
+	}
+
+}
